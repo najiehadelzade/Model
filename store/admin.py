a50b30b1b9820c20e55d4f1185fd0e05da67755a
Najieh Adelzadeh <najiehadelzade@gmail.com>
2023-12-15 12:18:14 +0330

    commit after run pre-commit

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Seller, Category, Product, Order, OrderItem
+from .models import Category, Order, OrderItem, Product, Seller
 
 admin.site.register(Seller)
 admin.site.register(Category)
